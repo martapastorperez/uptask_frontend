7798a5a55bda2fe9b6f29fa64f762f03d5016e15
martapastorperez <martapastorperez97@gmail.com>
2024-10-16 16:08:02 +0200

    feat: get projects

--- src/services/ProjectAPI.ts
+++ src/services/ProjectAPI.ts
@@ -1,6 +1,22 @@
 import { isAxiosError } from "axios";
 import api from "../lib/axios";
-import { ProjectFormData } from "../types";
+import { ProjectFormData, dashboaardProjectSchema } from "../types";
+
+
+export async function getProjects() {
+    try {
+        const {data}= await api.get('/projects') 
+        const response=dashboaardProjectSchema.safeParse(data)
+        if(response.success){
+             return response.data
+        }
+       
+    } catch (error) {
+        if (isAxiosError(error)&& error.response) {
+           throw new Error (error.response.data.error)
+        }        
+    }
+}
 
 export async function createProject(formData:ProjectFormData) {
     try {
@@ -10,9 +26,6 @@ export async function createProject(formData:ProjectFormData) {
     } catch (error) {
         if (isAxiosError(error)&& error.response) {
            throw new Error (error.response.data.error)
-        }
-        
-        
+        }        
     }
-    
 }
