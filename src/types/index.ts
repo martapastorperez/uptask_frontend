670fb563598abf66533fb3a4c2fcaba38de324b3
martapastorperez <martapastorperez97@gmail.com>
2024-11-04 18:25:06 +0100

    feat: create page register and login

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,18 @@
 import { z } from "zod";
 
+/**Auth and users */
+const authSchema=z.object({
+    name:z.string(),
+    email: z.string().email(),
+    password:z.string(),
+    password_confirmation:z.string()
+})
+
+type Auth=z.infer<typeof authSchema>
+export type UserLoginForm=Pick<Auth, 'email'|'password'>
+export type UserRegistrationForm=Pick<Auth, 'name'|'email'|'password'|'password_confirmation'>
+
+
 /**Tasks */
 export const taskStatusSchema=z.enum(["pending" , "onHold" , "inProgress" , "underReview" , "completed"])
 export type taskStatus=z.infer<typeof taskStatusSchema>
