a6a687e0aca6ec9e560434ee906a2e0837fb7e5a
martapastorperez <martapastorperez97@gmail.com>
2024-12-02 17:33:52 +0100

    feat: delete note

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,13 +1,47 @@
+import { useAuth } from "@/hooks/useAuth"
+import { deleteNote } from "@/services/NoteAPI"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/Utils"
-
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMemo } from "react"
+import { useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type NoteDetailProps={
     note:Note
   }
 
 export const NoteDetail = ({note}:NoteDetailProps) => {
-  return (
+
+    const {data, isLoading}=useAuth()
+    const canDelete=useMemo(()=>data?._id===note.createdBy._id,[data])
+
+    const queryParams=new URLSearchParams(location.search)
+    const taskId=queryParams.get('viewTask')!
+    const params=useParams()
+    const projectId=params.projectId!
+
+    const queryClient=useQueryClient()
+
+    const {mutate}=useMutation({
+        mutationFn:deleteNote,
+        onError:(error)=>{
+            toast.error(error.message)
+        },
+        onSuccess:(data)=>{
+            toast.success(data)
+            queryClient.invalidateQueries({queryKey:['task', taskId]})
+        }
+    })
+
+    const handleDelete=(noteId:Note['_id'])=>{
+        const data={projectId, taskId, noteId}
+        mutate(data)
+    }
+
+    if (isLoading) return 'Cargando...'
+
+  if(data) return (
     <div className="p-3 flex justify-between items-center">
         <div>
             <p>
@@ -18,13 +52,13 @@ export const NoteDetail = ({note}:NoteDetailProps) => {
             </p>
         </div>
 
-        
-            <button
+        {canDelete &&(
+             <button
                 type="button"
                 className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
-                
+                onClick={()=>handleDelete(note._id)}
             >Eliminar</button>
-        
+        )}
     </div>
   )
 }
--- src/services/NoteAPI.ts
+++ src/services/NoteAPI.ts
@@ -1,17 +1,29 @@
 import api from "@/lib/axios"
-import { NoteFormData, Project, Task } from "../types"
+import { Note, NoteFormData, Project, Task } from "../types"
 import { isAxiosError } from "axios"
 
 
 type noteAPIType={
     projectId:Project['_id'],
     formData:NoteFormData,
-    taskId:Task['_id']
+    taskId:Task['_id'],
+    noteId:Note['_id']
 }
 
 export async function createNote({projectId, taskId, formData}:Pick<noteAPIType, 'projectId'|'taskId'|'formData'>) {
     try {
         const {data}= await api.post(`/projects/${projectId}/task/${taskId}/notes`, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error)&& error.response) {
+           throw new Error (error.response.data.error)
+        }        
+    }
+}
+
+export async function deleteNote({projectId, taskId, noteId}:Pick<noteAPIType, 'projectId'|'taskId'|'noteId'>) {
+    try {
+        const {data}= await api.delete(`/projects/${projectId}/task/${taskId}/notes/${noteId}`)
         return data
     } catch (error) {
         if (isAxiosError(error)&& error.response) {
