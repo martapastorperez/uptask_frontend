c5474e685eb3eddea5a7a91a9e6e8d27aa82d695
martapastorperez <martapastorperez97@gmail.com>
2024-12-03 18:27:11 +0100

    feat: update password profile

--- src/services/ProfileAPI.ts
+++ src/services/ProfileAPI.ts
@@ -1,10 +1,21 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { UserProfileForm } from "../types";
+import { UpdateCurrentUserPasswordForm, UserProfileForm } from "../types";
 
 export async function updateProfile(formData:UserProfileForm) {
     try {
         const {data}= await api.put<string>(`/auth/profile`, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error)&& error.response) {
+            throw new Error (error.response.data.error)
+         }    
+    }
+}
+
+export async function updatePassword(formData:UpdateCurrentUserPasswordForm) {
+    try {
+        const {data}= await api.post<string>(`/auth/update-password`, formData)
         return data
     } catch (error) {
         if (isAxiosError(error)&& error.response) {
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 const authSchema=z.object({
     name:z.string(),
     email: z.string().email(),
+    current_password:z.string(),
     password:z.string(),
     password_confirmation:z.string(),
     token:z.string()
@@ -16,6 +17,7 @@ export type ConfirmToken=Pick<Auth,'token'>
 export type RequestConfirmationCodeForm=Pick<Auth, 'email'>
 export type ForgotPasswordForm=Pick<Auth, 'email'>
 export type NewPasswordFormType=Pick<Auth, 'password'|'password_confirmation'>
+export type UpdateCurrentUserPasswordForm=Pick<Auth, 'current_password'|'password'|'password_confirmation'>
 
 /**Users */
 export const userSchema=authSchema.pick({
